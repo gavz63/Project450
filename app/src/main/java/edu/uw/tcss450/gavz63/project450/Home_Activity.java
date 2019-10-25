17e4c05f5e021759704d829196d5369b4142ccec
Gavin <gavz63@uw.edu>
2019-10-24 21:36:56 -0700

    Layout and client side validation for login fragment. Navigates to home activity and to register fragment properly

--- app/src/main/java/edu/uw/tcss450/gavz63/project450/Home_Activity.java
+++ app/src/main/java/edu/uw/tcss450/gavz63/project450/Home_Activity.java
@@ -24,6 +24,7 @@ import android.view.Menu;
 public class Home_Activity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private String mJwToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
